a32b35f1aa26a7fcd862cf2e32add13f9f22475c
William Snider <wsnider@my.dom.edu>
2025-03-16 22:06:46 -0500

    Update cat_trap_algorithms.py

--- cat_trap_algorithms.py
+++ cat_trap_algorithms.py
@@ -8,6 +8,8 @@ He mentioned that users were having trouble installing and setting up the enviro
 which is why he created the revised version.  I ultimately found success getting the game to run by using GitHub Codespaces.  
 I was able to play the game.  In order to run the game, one must run the main.py file and use Control + Shift + P, "Start Cat Trap Game".
 You may need to run main.py from my Repository 2, which is entitled Week 1 Cat Game Code Assignment Repo 2.
+
+CatGame.py in Repo 1 contains my evaluation function code.
 """
 
 import random
